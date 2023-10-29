92be28a83485db9cbaae8ac0d00fcbdbf985df45
88141glr <88141@glr.nl>
2023-10-29 12:18:38 +0100

    Fix 2

--- ADMIN/adminupload.php
+++ ADMIN/adminupload.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="../index.php" class="nav-link px-2 text-white">Home</a></li>
-          <li><a href="../AboutMe/AboutMe.php" class="nav-link px-2 text-white">Info</a></li>
+          <li><a href="../info.php" class="nav-link px-2 text-white">Info</a></li>
           <li><a href="../Cards/collectie.php" class="nav-link px-2 text-white">Cards</a></li>
         </ul>
 
